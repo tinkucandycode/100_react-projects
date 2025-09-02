cce57d4fcc0c6993366c4283a7994c21a2cd67cc
tinku candy <tinkucandy25@gmail.com>
2025-09-02 22:00:30 +0530

    02 avatar generator created

---
+++ 2-Avatar-generator/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-700 to-slate-900 flex items-center justify-center">
+        <div className="max-w-md bg-rose-50 w-full ">dsfsdf</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
